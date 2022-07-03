b420fc8f31d80a580bada25a131241b25a644a55
Reza Rizki <rezarizki77@gmail.com>
2022-07-03 14:44:34 +0700

    .

--- app/Http/Controllers/DaftarBarangController.php
+++ app/Http/Controllers/DaftarBarangController.php
@@ -24,7 +24,7 @@ class DaftarBarangController extends Controller
      */
     public function create()
     {
-        //
+        return view('pelanggan.plggn-daftarbarangpelanggan');
     }
 
     /**
